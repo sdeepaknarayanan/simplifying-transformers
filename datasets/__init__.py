8c4dfbc4c822476ebf5892ecacc05ee338c5eea3
nstalder13 <116730451+nstalder13@users.noreply.github.com>
2022-12-29 16:09:21 +0100

    Add files via upload

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,11 +1,14 @@
 from datasets.vocabulary import WordVocab
 from datasets.wikitext2 import WikiText2Dataset
+from datasets.BlockDataset import BlockDataset
 
 
 def get(dataset_name: str):
     match dataset_name:
         case 'wikitext2':
             return WikiText2Dataset
+        case 'block_training':
+            return BlockDataset
         case _:
             raise NotImplementedError
 
