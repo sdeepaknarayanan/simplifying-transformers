16acf5ca9d67fb8d4938cb68fd24c42e791bb5f4
nstalder13 <116730451+nstalder13@users.noreply.github.com>
2023-01-11 18:10:54 +0100

    Add files via upload

--- config/train_config.py
+++ config/train_config.py
@@ -1,6 +1,7 @@
 from config.base_config import BaseConfig
 from config.block_config import BlockConfig
 from config.merge_config import MergeConfig
+from config.mlp_config import MLPConfig
 
 
 class TrainConfig(BaseConfig):
@@ -69,7 +70,7 @@ class BlockTrainConfig(BlockConfig):
 class MergeTrainConfig(BlockConfig):
 
     def __init__(self):
-        super(BlockConfig).__init__()
+        super(MergeConfig).__init__()
         self.name = 'training options'
         parser = MergeConfig().get_parser()
 
@@ -79,7 +80,7 @@ class MergeTrainConfig(BlockConfig):
                             help="this checkpoint is loaded to the teacher before training")
         parser.add_argument('--epochs', type=int, default=15, help='Number of epochs to train for.')
 
-        parser.add_argument("--lr", type=float, default=1e-5, help="learning rate of adam")
+        parser.add_argument("--lr", type=float, default=1e-6, help="learning rate of adam")
         parser.add_argument("--adam_weight_decay", type=float, default=0.01, help="weight_decay of adam")
         parser.add_argument("--adam_beta1", type=float, default=0.9, help="adam first beta value")
         parser.add_argument("--adam_beta2", type=float, default=0.999, help="adam first beta value")
@@ -97,4 +98,38 @@ class MergeTrainConfig(BlockConfig):
 
     def get(self):
         return self.parser.parse_args()
+
+
+class MLPTrainConfig(BlockConfig):
+
+    def __init__(self):
+        super(MLPConfig).__init__()
+        self.name = 'training options'
+        parser = MLPConfig().get_parser()
+
+        parser.add_argument('--train', dest='train', action='store_true')
+        parser.set_defaults(train=True)
+        parser.add_argument('--model_checkpoint', type=str, default="models/_checkpoints/wikitext2/BERTLM-latest.pth",
+                            help="this checkpoint is loaded to the teacher before training")
+        parser.add_argument('--epochs', type=int, default=15, help='Number of epochs to train for.')
+
+        parser.add_argument("--lr", type=float, default=1e-6, help="learning rate of adam")
+        parser.add_argument("--adam_weight_decay", type=float, default=0.01, help="weight_decay of adam")
+        parser.add_argument("--adam_beta1", type=float, default=0.9, help="adam first beta value")
+        parser.add_argument("--adam_beta2", type=float, default=0.999, help="adam first beta value")
+        parser.add_argument("--warmup_steps", type=int, default=0, help="warmup steps before weight decay.")
+
+        parser.add_argument("--block", type=int, default=1, help="Layer in which the block to train is")
+
+        parser.add_argument('--save_checkpoint_every', type=int, default=10, help='specify how often a lasting'
+                                                                                 'checkpoint should be stored.')
+
+        self.parser = parser
+
+    def get_parser(self):
+        return self.parser
+
+    def get(self):
+        return self.parser.parse_args()
+
 
